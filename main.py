665fd7c095bfaff731c3856ae64394bb535d6221
mike820324 <mike820324@gmail.com>
2016-04-16 21:52:04 +0800

    Remove uncessary import, small bug fix and using logger.

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import socket
 import datetime, time
 import tornado.ioloop
 import tornado.tcpserver
-import tornado.tcpclient
 import tornado.iostream
 
 from http import HttpRequest, HttpResponse
+
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 class HttpLayer(object):
     '''
@@ -29,33 +32,34 @@ class HttpLayer(object):
         self.target_dest_stream.set_close_callback(self.on_dest_close)
 
     def on_src_close(self):
-        self.is_src_close = True
+        # fixme: need better error handling
+        pass
 
     def on_dest_close(self):
-        self.is_dest_close = True
+        # fixme: need better error handling
+        pass
 
     def on_request(self, data):
         self.http_request.parse(data)
-        if self.http_request.is_done and not self.is_dest_close:
-            current_time = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-            print "{0} : {1} {2}".format(current_time, self.dest_ip, self.http_request.url)
+        if self.http_request.is_done and not self.target_dest_stream.closed():
+            __data = self.http_request.serialize()
+            self.http_request.deserialize(__data)
             self.target_dest_stream.write(self.http_request.data)
-
             self.http_request.clear()
 
     def on_response(self, data):
         self.http_response.parse(data)
-        if self.http_response.is_done and not self.is_src_close:
+        if self.http_response.is_done and not self.target_src_stream.closed():
             current_time = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-            print "{0} : {1} {2}".format(current_time, self.dest_ip, self.http_response.status_str)
-
+            # wait for zmq reply
             for chunk in self.http_response.data:
                 try:
                     self.target_src_stream.write(chunk)
                 except tornado.iostream.StreamClosedError(real_error):
-                    self.on_dest_close()
-
+                    self.on_src_close()
+            logger.info("{0} : {1} {2} {3} {4}".format(current_time, self.dest_ip, self.http_response.status, self.http_request.method, self.http_request.url))
             self.http_response.clear()
+            logger.info(self.http_response.serialize())
 
 class TLSLayer(object):
     '''
@@ -138,38 +142,39 @@ class SocksLayer(object):
         self.dest_stream = None
     
     def on_socks_greeting(self, data):
-        print "socks client greeting"
+        logger.info("socks greeting to {0}".format(self.src_stream.socket.getpeername()[0]))
         socks_init_data = struct.unpack('BBB', data)
         socks_version = socks_init_data[0]
         socks_nmethod = socks_init_data[1]
         socks_methods = socks_init_data[2]
     
-        if socks_nmethod != 1:
-            print "Auth not supported"
-        
-        response = struct.pack('BB', 5, 0)
-        self.src_stream.write(response)
-        self.src_stream.read_bytes(10, self.on_socks_request)
+        if socks_nmethod != 1: 
+            logger.warning("SOCKS5 Auth not supported")
+            self.src_stream.close()
+        else: 
+            response = struct.pack('BB', 5, 0)
+            self.src_stream.write(response)
+            self.src_stream.read_bytes(10, self.on_socks_request)
 
     def on_socks_request(self, data):
-        # print "socks client request"
         self.request_data = struct.unpack('!BBxBIH', data)
         socks_version = self.request_data[0]
         socks_cmd = self.request_data[1]
         socks_atyp = self.request_data[2]
-        socks_dest_addr = self.request_data[3]
+        socks_dest_addr = socket.inet_ntoa(struct.pack('!I', self.request_data[3]))
         socks_dest_port = self.request_data[4]
+        logger.info("socks request to {0}:{1}".format(socks_dest_addr, socks_dest_port))
 
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
         self.dest_stream = tornado.iostream.IOStream(s)
-        self.dest_stream.connect((socket.inet_ntoa(struct.pack('!I', socks_dest_addr)), socks_dest_port), self.on_dest_connect)
+        self.dest_stream.connect((socks_dest_addr, socks_dest_port), self.on_dest_connect)
 
     def on_dest_connect(self):
         socks_dest_port = self.request_data[4]
         response = struct.pack('!BBxBIH', 5, 0, 1, self.request_data[-2], self.request_data[-1])
         self.src_stream.write(response)
-        # print "socks -> http"
 
+        # fixme: Determine in runtime.
         if socks_dest_port == 5000 or socks_dest_port == 80:
             HttpLayer(self.src_stream, self.dest_stream)
 
@@ -184,17 +189,18 @@ class ProxyServer(tornado.tcpserver.TCPServer):
         super(ProxyServer, self).__init__()
 
     def handle_stream(self, stream, port):
-        if port == 6680:
-            print "ok it's good"
         SocksLayer(stream)
 
 def main():
     server = ProxyServer()
+
+    # fixme: use options
     server.listen(5580, "127.0.0.1")
 
     try:
         tornado.ioloop.IOLoop.instance().start()
     except KeyboardInterrupt:
-        print "bye..."
+        logger.info("bye")
 
-main()
+if __name__ == "__main__":
+    main()
