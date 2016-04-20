c51202a62486ede53a11dde7e1140e13b4e9ce4d
han-pc <chhsiao90@gmail.com>
2016-04-20 15:46:08 +0800

    update main function to start up connection controller with cc, not ss

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import connection_controller
 
 def main():
     parser = argparse.ArgumentParser(description="")
-    parser.add_argument("service", help="define which service to start: proxy,ss")
+    parser.add_argument("service", help="define which service to start: proxy,cc")
     parser.add_argument("--host", default="127.0.0.1")
     parser.add_argument("--port", type=int, default=5580)
     args = parser.parse_args()
     if args.service == "proxy":
         proxy.start_proxy_server(args.host, args.port)
-    if args.service == "ss":
+    if args.service == "cc":
         connection_controller.start_server()
 
 if __name__ == "__main__":
