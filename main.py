95f3fc291cb816dd525df25a56958e820019735c
mike820324 <mike820324@gmail.com>
2016-04-24 17:59:49 +0800

    Enhance command line options

--- main.py
+++ main.py
@@ -3,19 +3,32 @@ from microproxy import proxy
 from microproxy.viewer import log as log_viewer
 
 
-def main():
-    parser = argparse.ArgumentParser(description="")
-    parser.add_argument("service", help="define which service to start: proxy,sub")
-    parser.add_argument("--host", default="127.0.0.1")
-    parser.add_argument("--port", type=int, default=5580)
-    parser.add_argument("--viewer_type", default="log")
-    args = parser.parse_args()
-    if args.service == "proxy":
+def proxy_handler(args):
         proxy.start_proxy_server(args.host, args.port)
 
-    if args.service == "viewer":
-        if args.viewer_type == "log":
-            log_viewer.start()
+
+def viewer_handler(args):
+    if args.viewer_type == "log":
+        log_viewer.start()
+
+
+def main():
+    parser = argparse.ArgumentParser(description="")
+    subparser = parser.add_subparsers()
+
+    proxy_parser = subparser.add_parser('proxy', help="Enable Proxy Server")
+    proxy_parser.add_argument("--host", default="127.0.0.1")
+    proxy_parser.add_argument("--port", type=int, default=5580)
+    proxy_parser.set_defaults(func=proxy_handler)
+
+    viewer_parser = subparser.add_parser("viewer", help="Open Viewer")
+    viewer_parser.add_argument("--viewer_type",
+                               choices=["log"],
+                               default="log")
+    viewer_parser.set_defaults(func=viewer_handler)
+
+    args = parser.parse_args()
+    args.func(args)
 
 if __name__ == "__main__":
     main()
