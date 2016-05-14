a4604b4d52bfb201e1826079f981ceaee0ff1c3e
mike820324 <mike820324@gmail.com>
2016-05-14 15:22:08 +0800

    Remove HttpMessage since we no longer need it.

--- microproxy/http.py
+++ microproxy/http.py
@@ -22,13 +22,13 @@ class HttpRequest(object):
         else:
             self.headers = headers
 
-    def to_json(self):
+    def serialize(self):
         json = {}
         json["version"] = self.version
         json["method"] = self.method
         json["path"] = self.path
         json["body"] = self.body.encode("base64")
-        json["headers"] = _serialize_headers(self.headers)
+        json["headers"] = {k: v for k, v in self.headers.get_all()}
         return json
 
 
@@ -52,42 +52,11 @@ class HttpResponse(object):
     def append_body(self, chunk):
         self.body += bytes(chunk)
 
-    def to_json(self):
+    def serialize(self):
         json = {}
         json["version"] = self.version
         json["code"] = self.code
         json["reason"] = self.reason
         json["body"] = self.body.encode("base64")
-        json["headers"] = _serialize_headers(self.headers)
+        json["headers"] = {k: v for k, v in self.headers.get_all()}
         return json
-
-
-class HttpMessage(object):
-    def __init__(self,
-                 version="",
-                 status="",
-                 method="",
-                 url="",
-                 path="",
-                 query_string="",
-                 header=None,
-                 body=b""):
-        super(HttpMessage, self).__init__()
-        self.version = version
-        self.status = status
-        self.method = method
-        self.url = url
-        self.path = path
-        self.query_string = query_string
-        if header is None:
-            self.header = {}
-        else:
-            self.header = header
-        self.body = body
-
-
-def _serialize_headers(headers):
-    new_headers = {}
-    for (k, v) in headers.get_all():
-        new_headers[k] = v
-    return new_headers
