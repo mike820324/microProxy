b117368ebe98616b621a1182a630f73196edff3c
chhsiao90 <chhsiao90@gmail.com>
2016-05-16 14:57:46 +0800

    refactor proxy.py and remove outdated test

--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -1,12 +1,8 @@
 import platform
 import struct
-import datetime
-import sys
 from mock import Mock, PropertyMock, call
 from tornado.concurrent import Future
-import tornado.testing
-from tornado.testing import AsyncTestCase
-import tornado.gen
+from tornado.testing import AsyncTestCase, gen_test
 
 from microproxy import proxy
 
@@ -16,7 +12,7 @@ class SocksProxyHandlerTest(AsyncTestCase):
         super(SocksProxyHandlerTest, self).setUp()
         self.handler = proxy.SocksProxyHandler()
 
-    @tornado.testing.gen_test
+    @gen_test
     def test_read_and_return_addr(self):
         stream_source = Mock()
         stream_source.side_effect = [
@@ -51,7 +47,7 @@ class TranparentProxyHandlerTest(AsyncTestCase):
         super(TranparentProxyHandlerTest, self).setUp()
         self.handler = proxy.TranparentProxyHandler()
 
-    @tornado.testing.gen_test
+    @gen_test
     def test_read_and_return_addr(self):
         if platform.system() == "Linux":
             socket = Mock()
@@ -73,34 +69,6 @@ class TranparentProxyHandlerTest(AsyncTestCase):
 class HttpLayerTest(AsyncTestCase):
     def setUp(self):
         super(HttpLayerTest, self).setUp()
-        self.context = proxy.Context(Mock(), Mock(), Mock())
-        self.http_layer = proxy.HttpLayer(self.context)
-
-    @tornado.testing.gen_test
-    def test_process_success(self):
-        self.context.src_stream.read_bytes = Mock(return_value=_create_future(
-            b"GET / HTTP/1.1\r\n" +
-            b"host: github.com\r\n\r\n"))
-        self.context.src_stream.write = Mock(return_value=_create_future(None))
-        self.context.dest_stream.read_bytes = Mock(side_effect=[
-            _create_future(
-                b"HTTP/1.1 200 OK\r\n" +
-                b"Content-Type: text/html\r\n\r\n" +
-                b"<html></html>"),
-            _create_future(b"")
-        ])
-        self.context.dest_stream.write = Mock(return_value=_create_future(None))
-
-        self.context.interceptor.request = Mock()
-        self.context.interceptor.response = Mock()
-        self.context.interceptor.record = Mock()
-
-        yield tornado.gen.with_timeout(datetime.timedelta(seconds=1), self.http_layer.process())
-        self.context.src_stream.read_bytes.assert_called_with(sys.maxsize, partial=True)
-        self.context.dest_stream.read_bytes.assert_called_with(sys.maxsize, partial=True)
-        assert self.context.interceptor.request.called
-        assert self.context.interceptor.response.called
-        assert self.context.interceptor.record.called
 
 
 def _create_future(result):
