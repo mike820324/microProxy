9cc99071d32eeac9c9e0bb584c90397b046aafe5
mike820324 <mike820324@gmail.com>
2016-05-17 20:46:14 +0800

    Use blinker to have a better control flow. Close Issue #48

--- microproxy/layer/http1.py
+++ microproxy/layer/http1.py
@@ -2,6 +2,7 @@ from tornado import iostream
 from tornado import gen
 from tornado import httputil
 from tornado import http1connection
+from blinker import signal
 
 from microproxy.utils import get_logger
 from microproxy import http
@@ -13,6 +14,7 @@ class Http1Layer(httputil.HTTPServerConnectionDelegate):
     def __init__(self, context):
         super(Http1Layer, self).__init__()
         self.context = context
+        self.http_forwarder = None
 
     def process(self):
         http_server_connection = http1connection.HTTP1ServerConnection(self.context.src_stream)
@@ -21,10 +23,10 @@ class Http1Layer(httputil.HTTPServerConnectionDelegate):
     def start_request(self, server_conn, request_conn):
         dest_conn = http1connection.HTTP1Connection(self.context.dest_stream,
                                                     True)
-        http_forwarder = HttpForwarder(self.context,
-                                       request_conn,
-                                       dest_conn)
-        return http_forwarder.create_req_reader()
+        self.http_forwarder = HttpForwarder(self.context,
+                                            request_conn,
+                                            dest_conn)
+        return self.http_forwarder.create_req_reader()
 
     def on_close(self, server_conn):
         self.context.dest_stream.close()
@@ -32,10 +34,9 @@ class Http1Layer(httputil.HTTPServerConnectionDelegate):
 
 
 class HttpReqReader(httputil.HTTPMessageDelegate):
-    def __init__(self, context, http_forwarder):
+    def __init__(self, context):
         super(HttpReqReader, self).__init__()
         self.context = context
-        self.http_forwarder = http_forwarder
         self._chunks = []
 
     def headers_received(self, start_line, headers):
@@ -51,11 +52,11 @@ class HttpReqReader(httputil.HTTPMessageDelegate):
 
     def finish(self):
         self.req.body = b"".join(self._chunks)
-        self.http_forwarder.req_done(self.req)
+        signal("request_done").send(self)
 
     def on_connection_close(self):
         logger.debug("source connection close")
-        self.http_forwarder.close_dest_conn()
+        signal("disconnect").send(self)
 
 
 class HttpForwarder(object):
@@ -68,26 +69,36 @@ class HttpForwarder(object):
         self.resp = None
 
     def create_req_reader(self):
-        return HttpReqReader(self.context,
-                             self)
+        reader = HttpReqReader(self.context)
+        done_signal = signal("request_done")
+        disconnect_signal = signal("disconnect")
+
+        done_signal.connect(self.req_done, sender=reader)
+        disconnect_signal.connect(self.close_dest_conn, sender=reader)
+        return reader
 
     def create_resp_reader(self):
-        return HttpRespReader(self.context,
-                              self)
+        reader = HttpRespReader(self.context)
+        done_signal = signal("response_done")
+        disconnect_signal = signal("disconnect")
+
+        done_signal.connect(self.resp_done, sender=reader)
+        disconnect_signal.connect(self.close_src_conn, sender=reader)
+        return reader
 
     @gen.coroutine
-    def req_done(self, req):
+    def req_done(self, sender):
         logger.debug("source request done")
-        self.req = req
+        self.req = sender.req
         if self.context.interceptor:
-            self.context.interceptor.request(req)
+            self.context.interceptor.request(self.req)
 
-        status_line = httputil.RequestStartLine(req.method,
-                                                req.path,
-                                                req.version)
+        status_line = httputil.RequestStartLine(self.req.method,
+                                                self.req.path,
+                                                self.req.version)
         try:
-            yield self.dest_conn.write_headers(status_line, req.headers)
-            yield self.dest_conn.write(req.body)
+            yield self.dest_conn.write_headers(status_line, self.req.headers)
+            yield self.dest_conn.write(self.req.body)
             yield self.dest_conn.read_response(self.create_resp_reader())
             logger.debug("destination request done")
         except iostream.StreamClosedError:
@@ -97,25 +108,25 @@ class HttpForwarder(object):
             logger.exception(e)
 
     @gen.coroutine
-    def resp_done(self, resp):
+    def resp_done(self, sender):
         logger.debug("destination response done")
-        self.resp = resp
+        self.resp = sender.resp
         if self.context.interceptor:
-            self.context.interceptor.response(resp)
+            self.context.interceptor.response(self.resp)
             self.context.interceptor.record(self.req, self.resp)
 
-        headers = resp.headers.copy()
+        headers = self.resp.headers.copy()
         # restriction on using tornado http connection
         # pass Transfer-Encoding in header will let the source cannot receive chunks response correctly
         if headers.get("Transfer-Encoding"):
             del headers["Transfer-Encoding"]
 
-        status_line = httputil.ResponseStartLine(resp.version,
-                                                 resp.code,
-                                                 resp.reason)
+        status_line = httputil.ResponseStartLine(self.resp.version,
+                                                 self.resp.code,
+                                                 self.resp.reason)
         try:
             yield self.src_conn.write_headers(status_line, headers)
-            yield self.src_conn.write(resp.body)
+            yield self.src_conn.write(self.resp.body)
             self.src_conn.finish()
             logger.debug("source response done")
         except iostream.StreamClosedError:
@@ -132,10 +143,9 @@ class HttpForwarder(object):
 
 
 class HttpRespReader(httputil.HTTPMessageDelegate):
-    def __init__(self, context, http_forwarder):
+    def __init__(self, context):
         super(HttpRespReader, self).__init__()
         self.context = context
-        self.http_forwarder = http_forwarder
         self._chunks = []
 
     def headers_received(self, start_line, headers):
@@ -151,8 +161,8 @@ class HttpRespReader(httputil.HTTPMessageDelegate):
 
     def finish(self):
         self.resp.body = b"".join(self._chunks)
-        self.http_forwarder.resp_done(self.resp)
+        signal("response_done").send(self)
 
     def on_connection_close(self):
         logger.debug("destination connection closed")
-        self.http_forwarder.close_src_conn()
+        signal("disconnect").send(self)
