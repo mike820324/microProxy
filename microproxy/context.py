01f374f08f041904923621cc08abfd118e9c5dad
chhsiao90 <chhsiao90@gmail.com>
2016-05-19 00:47:54 +0800

    move tls relative code to TlsLayer

--- microproxy/context.py
+++ microproxy/context.py
@@ -4,9 +4,25 @@ class Context(object):
                  dest_stream=None,
                  interceptor=None,
                  host=None,
-                 port=None):
+                 port=None,
+                 config=None):
         self.src_stream = src_stream
         self.dest_stream = dest_stream
         self.interceptor = interceptor
         self.host = host
         self.port = port
+        self.config = config
+
+    def new_context(self,
+                    src_stream=None,
+                    dest_stream=None):
+        if not src_stream:
+            src_stream = self.src_stream
+        if not dest_stream:
+            dest_stream = self.dest_stream
+        return Context(src_stream=src_stream,
+                       dest_stream=dest_stream,
+                       interceptor=self.interceptor,
+                       host=self.host,
+                       port=self.port,
+                       config=self.config)
