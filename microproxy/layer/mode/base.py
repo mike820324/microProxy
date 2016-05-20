fa12059b6b345828a7a3a197e3201a08926ba6f0
MicroMike <mike820324@gmail.com>
2016-05-20 15:46:13 +0800

    Clean up unused files

--- microproxy/layer/mode/base.py
+++
@@ -1,6 +0,0 @@
-class ProxyHandler(object):
-    def __init__(self):
-        super(ProxyHandler, self).__init__()
-
-    def create_dest_stream(self, host, port):
-        raise NotImplementedError()
