64198ce718435c49f88fdd39755e9049fe58c07c
chhsiao90 <chhsiao90@gmail.com>
2016-05-23 17:32:24 +0800

    add SrcStreamClosedError and DestStreamClosedError

--- microproxy/exception.py
+++ microproxy/exception.py
@@ -1,2 +1,13 @@
+from tornado import iostream
+
+
 class ProtocolError(Exception):
+    pass
+
+
+class SrcStreamClosedError(iostream.StreamClosedError):
+    pass
+
+
+class DestStreamClosedError(iostream.StreamClosedError):
     pass
