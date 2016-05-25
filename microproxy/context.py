989aa14880d35b9f3cb71d4b5e252292a29b4058
MicroMike <mike820324@gmail.com>
2016-05-25 13:55:12 +0800

    Implement alpn and sni for tls layer.

--- microproxy/context.py
+++ microproxy/context.py
@@ -2,12 +2,14 @@ class Context(object):
     def __init__(self,
                  src_stream=None,
                  dest_stream=None,
+                 schema=None,
                  host=None,
                  port=None,
                  config=None,
                  layer_manager=None):
         self.src_stream = src_stream
         self.dest_stream = dest_stream
+        self.schema = schema
         self.host = host
         self.port = port
         self.config = config
--- microproxy/layer/proxy/base.py
+++ microproxy/layer/proxy/base.py
@@ -1,8 +1,4 @@
 import socket
-import datetime
-
-from tornado import gen
-from tornado import iostream
 
 from microproxy.utils import get_logger
 logger = get_logger(__name__)
@@ -16,13 +12,11 @@ class ProxyLayer(object):
     def process(self):
         raise NotImplementedError
 
-    @gen.coroutine
     def create_dest_stream(self, dest_addr_info):
         dest_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-        dest_stream = iostream.IOStream(dest_socket)
+        dest_socket.setblocking(True)
         try:
-            yield gen.with_timeout(datetime.timedelta(5), dest_stream.connect(dest_addr_info))
-            raise gen.Return(dest_stream)
-        except gen.TimeoutError:
-            logger.warning("Connect to Destination Timeout")
+            dest_socket.connect(dest_addr_info)
+            return dest_socket
+        except:
             raise
