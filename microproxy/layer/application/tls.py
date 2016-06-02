d7b6970504e426eb9c59890898e062446dcedbbb
MicroMike <mike820324@gmail.com>
2016-06-02 15:22:24 +0800

    Use pyOpenSSL for more flexible control over ssl. However, there are some changes that need to make.
    - override TcpServer._handle_connection to use our own iostream implementation.
    - override iostream.start_tls to apply pyOpenssl
    - override serveral methods in ssliostream to apply pyOpenssl exception.

--- microproxy/layer/application/tls.py
+++ microproxy/layer/application/tls.py
@@ -1,8 +1,8 @@
-import ssl
+from OpenSSL import SSL
 from copy import copy
 from tornado import gen
-from tornado.iostream import SSLIOStream
 
+from microproxy.iostream import MicroProxySSLIOStream
 from microproxy.utils import get_logger
 logger = get_logger(__name__)
 
@@ -14,60 +14,63 @@ class TlsLayer(object):
         super(TlsLayer, self).__init__()
         self.context = copy(context)
         self.ssl_sock = None
-        self.alpn_info = ""
 
-    def sni_callback(self, ssl_socket, server_hostname, ssl_context):
+    def src_alpn_callback(self, src_ssl_conn, alpn):
         try:
-            dest_context = self.create_dest_sslcontext()
-            self.ssl_sock = dest_context.wrap_socket(self.context.dest_stream,
-                                                     server_hostname=server_hostname)
+            dest_context = self.create_dest_sslcontext(alpn)
+            self.ssl_sock = SSL.Connection(dest_context,
+                                           self.context.dest_stream)
+            self.ssl_sock.set_tlsext_host_name(self.context.host)
+            self.ssl_sock.set_connect_state()
+            self.ssl_sock.do_handshake()
 
-            if ssl.HAS_ALPN:
-                self.alpn_info = self.ssl_sock.selected_alpn_protocol()
-                if self.alpn_info == 'h2':
-                    logger.debug("alpn using h2")
-                    ssl_context.set_alpn_protocols(['h2'])
-                else:
-                    logger.debug("alpn using http/1.1")
-                    ssl_context.set_alpn_protocols(['http/1.1'])
+            alpn_info = self.ssl_sock.get_alpn_proto_negotiated()
+
+            if alpn_info == "http/1.1":
+                self.context.schema = "https"
+            elif alpn_info == "h2":
+                self.context.schema = "h2"
+            else:
+                self.context.schema = "https"
+            return bytes(alpn_info)
 
         except Exception as e:
             logger.exception(e)
-            return ssl.ALERT_DESCRIPTION_HANDSHAKE_FAILURE
+            raise
         return None
 
-    def create_dest_sslcontext(self):
-        dest_ssl_context = ssl.create_default_context()
-        if ssl.HAS_ALPN:
-            dest_ssl_context.set_alpn_protocols(self.SUPPORT_PROTOCOLS)
-        if ssl.HAS_NPN:
-            dest_ssl_context.set_npn_protocols(self.SUPPORT_PROTOCOLS)
+    def src_sni_callback(self, src_ssl_conn):
+        self.context.host = src_ssl_conn.get_servername()
+
+    def create_dest_sslcontext(self, alpn):
+        dest_ssl_context = SSL.Context(SSL.SSLv23_METHOD)
+        dest_ssl_context.set_alpn_protos(alpn)
 
         return dest_ssl_context
 
     def create_src_sslcontext(self):
-        context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
-        context.load_cert_chain(self.context.config["certfile"],
-                                self.context.config["keyfile"])
-        context.set_servername_callback(self.sni_callback)
+        context = SSL.Context(SSL.SSLv23_METHOD)
+        context.use_certificate_file(self.context.config["certfile"])
+        context.use_privatekey_file(self.context.config["keyfile"])
+        context.set_tlsext_servername_callback(self.src_sni_callback)
+        context.set_alpn_select_callback(self.src_alpn_callback)
 
         return context
 
     @gen.coroutine
     def process(self):
         src_ssl_context = self.create_src_sslcontext()
-        src_stream = yield self.context.src_stream.start_tls(server_side=True,
-                                                             ssl_options=src_ssl_context)
+        try:
+            src_stream = yield self.context.src_stream.start_tls(server_side=True,
+                                                                 ssl_options=src_ssl_context)
+        except Exception as e:
+            logger.exception(e)
+            raise
+
         self.ssl_sock.setblocking(False)
-        dest_stream = SSLIOStream(self.ssl_sock)
+        dest_stream = MicroProxySSLIOStream(self.ssl_sock)
 
         self.context.src_stream = src_stream
         self.context.dest_stream = dest_stream
 
-        if self.alpn_info == "http/1.1":
-            self.context.schema = "https"
-        elif self.alpn_info == "h2":
-            self.context.schema = "h2"
-        else:
-            self.context.schema = "https"
         raise gen.Return(self.context)
