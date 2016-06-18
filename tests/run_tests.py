c5756565533f47f289ef8cefba0ac8fd02352a7b
chhsiao90 <chhsiao90>
2016-06-18 17:07:09 +0800

    Implement tui unittest

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -7,7 +7,8 @@ TEST_MODULES = [
     "tests.test_proxy",
     "tests.layer.test_forward",
     "tests.layer.test_http1",
-    "tests.layer.test_socks"
+    "tests.layer.test_socks",
+    "tests.viewer.test_tui"
 ]
 
 
