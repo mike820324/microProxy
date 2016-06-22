55fb01f79af781c3908259b9f3f6142feeae9a95
MicroMike <mike820324@gmail.com>
2016-06-22 11:57:01 +0800

    Fix Issue #73 - Add timeout when connect to destination. Closed Issue #73.

--- microproxy/layer/proxy/base.py
+++ microproxy/layer/proxy/base.py
@@ -13,5 +13,8 @@ class ProxyLayer(object):
     def create_dest_stream(self, dest_addr_info):
         dest_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
         dest_socket.setblocking(True)
+        # FIXME: using concurrent.futures ThreadExecutor?
+        dest_socket.settimeout(3)
         dest_socket.connect(dest_addr_info)
+        dest_socket.settimeout(None)
         return dest_socket
