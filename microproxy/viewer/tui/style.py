50a9ccc50e60f574d56a75ab4e5529400b6fb9b9
chhsiao90 <chhsiao90@gmail.com>
2016-06-28 17:15:32 +0800

    Integrate with gviewer

--- microproxy/viewer/tui/style.py
+++
@@ -1,9 +0,0 @@
-palette = [
-    ("header", "white", "dark green", "bold"),
-    ("footer", "white", "dark blue", "bold"),
-    ("message", "white", "black"),
-    ("message focus", "black", "light gray"),
-    ("prop", "white", "black"),
-    ("prop focus", "black", "light gray"),
-    ("prop separator", "white", "dark red", "bold")
-]
