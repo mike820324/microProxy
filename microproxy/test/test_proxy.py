26a3091b95622d032afc10e2c921de874272ab10
mike820324 <mike820324@gmail.com>
2016-07-07 21:06:36 +0800

    Add new tests for layer manager.

--- microproxy/test/test_proxy.py
+++ microproxy/test/test_proxy.py
@@ -4,7 +4,8 @@ from tornado.testing import AsyncTestCase
 
 from microproxy.proxy import LayerManager
 from microproxy.context import Context
-from microproxy.layer import SocksLayer, TransparentLayer, Http1Layer, TlsLayer
+from microproxy.layer import SocksLayer, TransparentLayer, TlsLayer
+from microproxy.layer import Http1Layer, Http2Layer
 
 
 class LayerManagerTest(AsyncTestCase):
@@ -19,6 +20,25 @@ class LayerManagerTest(AsyncTestCase):
         }
         self.layer_manager = LayerManager(config=self.config)
         self.src_stream = Mock()
+
+    def test_get_socks_layer(self):
+        context = Context(src_stream=Mock(),
+                          config=self.config,
+                          port=443)
+
+        layer = self.layer_manager.get_first_layer(context)
+        self.assertIsInstance(layer, SocksLayer)
+
+    def test_get_transparent_layer(self):
+        context = Context(src_stream=Mock(),
+                          config=self.config,
+                          port=443)
+
+        config = self.config
+        config["mode"] = "transparent"
+        layer_manager = LayerManager(config=config)
+        layer = layer_manager.get_first_layer(context)
+        self.assertIsInstance(layer, TransparentLayer)
 
     def test_get_tls_layer(self):
         context = Context(src_stream=Mock(),
@@ -50,3 +70,13 @@ class LayerManagerTest(AsyncTestCase):
         tls_layer = TlsLayer(context, None)
         layer = self.layer_manager.next_layer(tls_layer, context)
         self.assertIsInstance(layer, Http1Layer)
+
+    def test_get_http2_layer(self):
+        context = Context(src_stream=Mock(),
+                          config=self.config,
+                          port=443)
+
+        context.scheme = "h2"
+        tls_layer = TlsLayer(context, None)
+        layer = self.layer_manager.next_layer(tls_layer, context)
+        self.assertIsInstance(layer, Http2Layer)
