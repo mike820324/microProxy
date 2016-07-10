ff1d1750d00fac5bb177ed2b63a5f0e0957e892b
mike820324 <mike820324@gmail.com>
2016-07-10 15:48:49 +0800

    Use a better way to get requirements for setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,15 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+with open(os.path.join(here, './requirements/proxy.txt')) as f:
+    proxy_deps = [dep for dep in f.read().split("\n") if dep]
+
+with open(os.path.join(here, './requirements/viewer.txt')) as f:
+    viewer_deps = [dep for dep in f.read().split("\n") if dep]
+
+with open(os.path.join(here, './requirements/development.txt')) as f:
+    dev_deps = [dep for dep in f.read().split("\n") if dep and "-r" not in dep]
+
 setup(
     name="microProxy",
     version="0.2.0",
@@ -42,26 +51,9 @@ setup(
             "microproxy=microproxy.command_line:main",
         ]
     },
-    install_requires=[
-        "tornado==4.3",
-        "pyzmq==15.2.0",
-        "blinker==1.4",
-        "ipaddress==1.0.16",
-        "watchdog==0.8.3",
-        "pyOpenSSL==16.0.0",
-        "service-identity==16.0.0",
-        "h2==2.4.0"
-    ],
+    install_requires=proxy_deps,
     extras_require={
-        'viewer': [
-            "colored==1.2.2",
-            "urwid==1.3.1",
-            "gviewer==1.1.0"
-        ],
-        'develop': [
-            "mock==2.0.0",
-            "coverage==4.0.3",
-            "coveralls==1.1"
-        ]
+        'viewer': viewer_deps,
+        'develop': dev_deps
     }
 )
