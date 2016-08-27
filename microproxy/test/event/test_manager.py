9324f2a96c97848d1f7b509e0caf950725a5796f
chhsiao90 <chhsiao90@gmail.com>
2016-08-27 17:40:39 +0800

    Add unit test for event manager, event client

---
+++ microproxy/test/event/test_manager.py
@@ -0,0 +1,23 @@
+import mock
+import unittest
+
+from microproxy.event import EventManager
+
+
+class EventManagerTest(unittest.TestCase):
+    def setUp(self):
+        self.handler = mock.Mock()
+        self.zmq_stream = mock.Mock()
+
+        self.event_manager = EventManager(
+            None, None, handler=self.handler, zmq_stream=self.zmq_stream)
+
+    def test_register_stream_call_invoked(self):
+        self.zmq_stream.on_recv.assert_called_with(self.event_manager._on_recv)
+
+    def test_recv(self):
+        self.event_manager._on_recv(['{"event": "yoyo"}'])
+        self.handler.handle_event.assert_called_with(dict(event="yoyo"))
+
+if __name__ == "__main__":
+    unittest.main()
