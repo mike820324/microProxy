20fd4cf160a0858a5ffdea6784e6385cef2479d8
chhsiao90 <chhsiao90@gmail.com>
2016-08-31 13:03:33 +0800

    Refine http2 exception

--- microproxy/exception.py
+++ microproxy/exception.py
@@ -15,3 +15,14 @@ class DestNotConnectedError(iostream.StreamClosedError):
 
 class DestStreamClosedError(iostream.StreamClosedError):
     pass
+
+
+class Http2Error(Exception):
+    def __init__(self, conn, error, error_msg, stream_id=None):
+        if stream_id:
+            err = "{0}: {1} on {2}, stream_id={3}, cause is {4}".format(
+                type(error), error_msg, conn, stream_id, error.args)
+        else:
+            err = "{0}: {1} on {2}, cause is {3}".format(
+                type(error), error_msg, conn, error.args)
+        super(Http2Error, self).__init__(err)
