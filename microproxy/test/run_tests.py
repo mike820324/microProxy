2036067eafadbd6c415d62460a4ac0d9d6c8c25e
chhsiao90 <chhsiao90@gmail.com>
2016-09-02 11:53:23 +0800

    First commit for http2 layer unittest

--- microproxy/test/run_tests.py
+++ microproxy/test/run_tests.py
@@ -9,6 +9,7 @@ TEST_MODULES = [
     "microproxy.test.layer.test_manager",
     "microproxy.test.layer.test_forward",
     "microproxy.test.layer.test_http1",
+    "microproxy.test.layer.test_http2",
     "microproxy.test.layer.test_socks",
     "microproxy.test.viewer.test_console",
     "microproxy.test.event.test_manager",
