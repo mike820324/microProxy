7346cc1646897916e19319587922a93ce8df2b76
mike820324 <mike820324@gmail.com>
2016-09-07 18:03:44 +0800

    Remove redundant code from main.py

--- main.py
+++ main.py
@@ -1,35 +1,4 @@
-from microproxy.config import parse_config
-from microproxy.command_line import create_options
-
-
-def main():
-    config_field_info = create_options()
-    config = parse_config(config_field_info)
-
-    if config["command_type"] == "proxy":
-        from microproxy.proxy import start_tcp_server
-        from microproxy.event import start_events_server
-        from microproxy.utils import get_logger, curr_loop
-        from microproxy.cert import init_cert_store
-        from microproxy.interceptor import init_interceptor
-
-        logger = get_logger(__name__)
-        init_cert_store(config)
-        init_interceptor(config)
-        start_tcp_server(config)
-        start_events_server(config)
-        try:
-            curr_loop().start()
-        except KeyboardInterrupt:
-            logger.info("bye")
-
-    elif config["command_type"] == "console-viewer":
-        from microproxy.viewer import console as console_viewer
-        console_viewer.start(config)
-
-    elif config["command_type"] == "tui-viewer":
-        from microproxy.viewer import tui as tui_viewer
-        tui_viewer.start(config)
+from microproxy.command_line import main
 
 if __name__ == '__main__':
     main()
