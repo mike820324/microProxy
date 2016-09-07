1d3532e5db8bc525735ac1eb67d5bb0f6024e6cd
mike820324 <mike820324@gmail.com>
2016-09-08 00:11:22 +0800

    Remove zmq creation logic from msg_publisher.

--- microproxy/interceptor/msg_publisher.py
+++ microproxy/interceptor/msg_publisher.py
@@ -1,4 +1,3 @@
-import zmq
 import json
 
 from microproxy.utils import get_logger
@@ -9,16 +8,9 @@ logger = get_logger(__name__)
 class MsgPublisher(object):
     TOPIC = "message"
 
-    def __init__(self, config, zmq_socket=None):
+    def __init__(self, config, zmq_socket):
         super(MsgPublisher, self).__init__()
-        self.zmq_socket = zmq_socket or self._create_socket(config["viewer_channel"])
-
-    def _create_socket(self, viewer_channel):
-        context = zmq.Context()
-        socket = context.socket(zmq.PUB)
-        socket.bind(viewer_channel)
-        logger.info("MsgPublisher is listening at {0}".format(viewer_channel))
-        return socket
+        self.zmq_socket = zmq_socket
 
     def publish(self, viewer_context):
         message = json.dumps(viewer_context.serialize())
