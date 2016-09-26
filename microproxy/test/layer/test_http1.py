990b873d99127ca14812a536ab36cf26e8a73915
mike820324 <mike820324@gmail.com>
2016-09-26 17:57:57 +0800

    Update unittest.
    - Move some shared logic into test/utils.py
    - Fix test error when refactor.

--- microproxy/test/layer/test_http1.py
+++ microproxy/test/layer/test_http1.py
@@ -1,13 +1,10 @@
-import socket
 from mock import Mock
 import h11
 
-from tornado.testing import AsyncTestCase, gen_test, bind_unused_port
+from tornado.testing import gen_test
 from tornado.gen import coroutine
-from tornado.locks import Semaphore
-from tornado.iostream import IOStream
-from tornado.netutil import add_accept_handler
 
+from microproxy.test.utils import ProxyAsyncTestCase
 from microproxy.context import LayerContext, ServerContext
 from microproxy.layer import Http1Layer
 from microproxy.protocol.http1 import Connection
@@ -15,7 +12,7 @@ from microproxy.context import HttpRequest, HttpResponse, HttpHeaders
 from microproxy.exception import SrcStreamClosedError, DestStreamClosedError
 
 
-class TestHttp1Layer(AsyncTestCase):
+class TestHttp1Layer(ProxyAsyncTestCase):
     def setUp(self):
         super(TestHttp1Layer, self).setUp()
         self.asyncSetUp()
@@ -24,28 +21,12 @@ class TestHttp1Layer(AsyncTestCase):
 
     @gen_test
     def asyncSetUp(self):
-        listener, port = bind_unused_port()
-        semaphore = Semaphore(0)
-        server_streams = []
-
-        def accept_callback(conn, addr):
-            server_stream = IOStream(conn)
-            server_streams.append(server_stream)
-            self.addCleanup(server_stream.close)
-            semaphore.release()
-
-        add_accept_handler(listener, accept_callback)
-        client_streams = [IOStream(socket.socket()), IOStream(socket.socket())]
-        for client_stream in client_streams:
-            self.addCleanup(client_stream.close)
-            yield [client_stream.connect(('127.0.0.1', port)),
-                   semaphore.acquire()]
-        self.io_loop.remove_handler(listener)
-        listener.close()
+        self.client_stream, src_stream = yield self.create_iostream_pair()
+        dest_stream, self.server_stream = yield self.create_iostream_pair()
 
         self.context = LayerContext(mode="socks",
-                                    src_stream=server_streams[0],
-                                    dest_stream=client_streams[1])
+                                    src_stream=src_stream,
+                                    dest_stream=dest_stream)
 
         interceptor = Mock()
         interceptor.publish = Mock(return_value=None)
@@ -53,17 +34,16 @@ class TestHttp1Layer(AsyncTestCase):
         interceptor.response = Mock(return_value=None)
         server_state = ServerContext(interceptor=interceptor)
 
-        self.src_stream = client_streams[0]
-        self.dest_stream = server_streams[1]
         self.http_layer = Http1Layer(server_state, self.context)
 
         self.client_conn = Connection(
-            h11.CLIENT, self.src_stream,
+            h11.CLIENT, self.client_stream,
             on_response=self.record_src_event,
             on_info_response=self.record_src_event,
             on_unhandled=self.ignore_event)
+
         self.server_conn = Connection(
-            h11.SERVER, self.dest_stream,
+            h11.SERVER, self.server_stream,
             on_request=self.record_dest_event,
             on_unhandled=self.ignore_event)
 
@@ -116,8 +96,8 @@ class TestHttp1Layer(AsyncTestCase):
 
         self.assertTrue(http_layer_future.running())
 
-        self.src_stream.close()
-        self.dest_stream.close()
+        self.client_stream.close()
+        self.server_stream.close()
         self.context.src_stream.close()
         self.context.dest_stream.close()
         yield http_layer_future
@@ -125,7 +105,7 @@ class TestHttp1Layer(AsyncTestCase):
     @gen_test
     def test_write_req_to_dest_failed(self):
         http_layer_future = self.http_layer.process_and_return_context()
-        self.dest_stream.close()
+        self.server_stream.close()
         self.client_conn.send_request(HttpRequest(
             version="HTTP/1.1", method="GET", path="/index",
             headers=[("Host", "localhost")]))
@@ -141,7 +121,7 @@ class TestHttp1Layer(AsyncTestCase):
             headers=[("Host", "localhost")]))
 
         self.assertTrue(http_layer_future.running())
-        self.dest_stream.close()
+        self.server_stream.close()
 
         with self.assertRaises(DestStreamClosedError):
             yield http_layer_future
@@ -163,7 +143,7 @@ class TestHttp1Layer(AsyncTestCase):
         self.assertEqual(request.path, "/index")
         self.assertEqual(request.headers, HttpHeaders([("Host", "localhost")]))
 
-        self.src_stream.close()
+        self.client_stream.close()
 
         self.server_conn.send_response(HttpResponse(
             version="HTTP/1.1", code="200", reason="OK",
@@ -267,12 +247,12 @@ class TestHttp1Layer(AsyncTestCase):
         self.assertEqual(request.path, "/index")
         self.assertEqual(request.headers, HttpHeaders([("Host", "localhost")]))
 
-        yield self.dest_stream.write(
+        yield self.server_stream.write(
             (b"HTTP/1.1 200 OK\r\n"
              b"Connection: closed\r\n\r\n"
              b"body"))
 
-        self.dest_stream.close()
+        self.server_stream.close()
 
         yield self.read_until_new_event(self.client_conn, self.src_events)
         self.assertEqual(len(self.src_events), 1)
@@ -307,7 +287,7 @@ class TestHttp1Layer(AsyncTestCase):
         self.assertEqual(request.path, "/chat")
         self.assertEqual(request.headers, HttpHeaders([("Host", "localhost"), ("Upgrade", "websocket")]))
 
-        self.src_stream.close()
+        self.client_stream.close()
 
         self.server_conn.send_info_response(HttpResponse(
             version="HTTP/1.1", code="101", reason="Switching Protocol",
@@ -317,7 +297,7 @@ class TestHttp1Layer(AsyncTestCase):
             yield http_layer_future
 
     def tearDown(self):
-        self.src_stream.close()
-        self.dest_stream.close()
+        self.client_stream.close()
+        self.server_stream.close()
         self.context.src_stream.close()
         self.context.dest_stream.close()
--- microproxy/test/protocol/test_http2.py
+++ microproxy/test/protocol/test_http2.py
@@ -1,17 +1,14 @@
-import socket
 import mock
 from datetime import timedelta
 from tornado import gen
-from tornado.testing import AsyncTestCase, gen_test, bind_unused_port
-from tornado.locks import Event
-from tornado.iostream import IOStream
-from tornado.netutil import add_accept_handler
+from tornado.testing import gen_test
 
+from microproxy.test.utils import ProxyAsyncTestCase
 from microproxy.protocol.http2 import Connection
 from microproxy.context import HttpRequest, HttpResponse, HttpHeaders
 
 
-class TestConnection(AsyncTestCase):
+class TestConnection(ProxyAsyncTestCase):
     def setUp(self):
         super(TestConnection, self).setUp()
         self.asyncSetUp()
@@ -25,21 +22,9 @@ class TestConnection(AsyncTestCase):
 
     @gen_test
     def asyncSetUp(self):
-        listener, port = bind_unused_port()
-        event = Event()
-
-        def accept_callback(conn, addr):
-            self.server_stream = IOStream(conn)
-            self.addCleanup(self.server_stream.close)
-            event.set()
-
-        add_accept_handler(listener, accept_callback)
-        self.client_stream = IOStream(socket.socket())
+        self.client_stream, self.server_stream = yield self.create_iostream_pair()
         self.addCleanup(self.client_stream.close)
-        yield [self.client_stream.connect(('127.0.0.1', port)),
-               event.wait()]
-        self.io_loop.remove_handler(listener)
-        listener.close()
+        self.addCleanup(self.server_stream.close)
 
     def on_request(self, stream_id, request, priority_updated):
         self.request = (stream_id, request)
