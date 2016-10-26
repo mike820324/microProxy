4a387ed8db395fe66308f6ca996250f24c19a2b3
mike820324 <mike820324@gmail.com>
2016-10-26 11:09:57 +0800

    Refine config.py and command line interface.
    - Add new option types.
    - Properly handle boolean type.
    - Change subcommand into individual binary.

--- microproxy/command_line.py
+++ microproxy/command_line.py
@@ -1,29 +1,4 @@
-from microproxy.config import parse_config, define_option, define_section
-
-
-def create_options():
-
-    config_field_info = {}
-
-    proxy_option_info = create_proxy_options()
-    define_section(config_field_info=config_field_info,
-                   section="proxy",
-                   option_info=proxy_option_info,
-                   help_str="Open microproxy service")
-
-    console_viewer_option_info = create_console_viewer_options()
-    define_section(config_field_info=config_field_info,
-                   section="console-viewer",
-                   option_info=console_viewer_option_info,
-                   help_str="Open console viewer")
-
-    tui_viewer_option_info = create_tui_viewer_options()
-    define_section(config_field_info=config_field_info,
-                   section="tui-viewer",
-                   option_info=tui_viewer_option_info,
-                   help_str="Open tui viewer")
-
-    return config_field_info
+from microproxy.config import parse_config, define_option
 
 
 def create_proxy_options():
@@ -32,86 +7,94 @@ def create_proxy_options():
                   option_name="host",
                   help_str="Specify the proxy host",
                   default="127.0.0.1",
-                  option_type="string",
-                  cmd_flags="--host")
+                  option_type="str",
+                  cmd_flags="--host",
+                  config_file_flags="proxy:host")
 
     define_option(option_info=proxy_option_info,
                   option_name="port",
                   help_str="Specify the proxy listening port",
                   default="5580",
                   option_type="int",
-                  cmd_flags="--port")
+                  cmd_flags="--port",
+                  config_file_flags="proxy:port")
 
     define_option(option_info=proxy_option_info,
                   option_name="mode",
                   help_str="Speficy the proxy mode, currently support socks proxy and transparent proxy",
-                  option_type="string",
+                  option_type="str",
                   cmd_flags="--mode",
+                  config_file_flags="proxy:mode",
                   choices=["socks", "transparent"])
 
     define_option(option_info=proxy_option_info,
                   option_name="http_port",
                   help_str="Add additional http port",
                   default="",
-                  option_type="list",
+                  option_type="list:int",
                   cmd_flags="--http-port",
-                  list_type="int")
+                  config_file_flags="proxy:http.port"
+                  )
 
     define_option(option_info=proxy_option_info,
                   option_name="https_port",
                   help_str="Add additional https port",
                   default="",
-                  option_type="list",
+                  option_type="list:int",
                   cmd_flags="--https-port",
-                  list_type="int")
+                  config_file_flags="proxy:https.port")
+
+    define_option(option_info=proxy_option_info,
+                  option_name="certfile",
+                  help_str="Specify the certificate file",
+                  option_type="str",
+                  cmd_flags="--cert-file",
+                  config_file_flags="proxy:certfile")
+
+    define_option(option_info=proxy_option_info,
+                  option_name="keyfile",
+                  help_str="Specify the private key file",
+                  option_type="str",
+                  cmd_flags="--key-file",
+                  config_file_flags="proxy:keyfile")
+
+    define_option(option_info=proxy_option_info,
+                  option_name="viewer_channel",
+                  help_str="Specify the viewer channel. ex. tcp://*:5581",
+                  option_type="str",
+                  cmd_flags="--viewer-channel",
+                  config_file_flags="channel:viewer")
+
+    define_option(option_info=proxy_option_info,
+                  option_name="events_channel",
+                  help_str="Specify the events channel. ex. tcp://*:5582",
+                  option_type="str",
+                  default="tcp://127.0.0.1:5582",  # Note: Make default to only accept local to access it
+                  cmd_flags="--events-channel",
+                  config_file_flags="channel:events")
 
     define_option(option_info=proxy_option_info,
                   option_name="plugins",
                   help_str="Load plugins",
                   default="",
-                  option_type="list",
+                  option_type="list:str",
                   cmd_flags="--plugins",
-                  list_type="string")
-
-    define_option(option_info=proxy_option_info,
-                  option_name="viewer_channel",
-                  help_str="Specify the viewer channel. ex. tcp://*:5581",
-                  option_type="string",
-                  cmd_flags="--viewer-channel")
-
-    define_option(option_info=proxy_option_info,
-                  option_name="certfile",
-                  help_str="Specify the certificate file",
-                  option_type="string",
-                  cmd_flags="--cert-file")
-
-    define_option(option_info=proxy_option_info,
-                  option_name="keyfile",
-                  help_str="Specify the private key file",
-                  option_type="string",
-                  cmd_flags="--key-file")
-
-    define_option(option_info=proxy_option_info,
-                  option_name="events_channel",
-                  help_str="Specify the events channel. ex. tcp://*:5582",
-                  option_type="string",
-                  default="tcp://127.0.0.1:5582",  # Note: Make default to only accept local to access it
-                  cmd_flags="--events-channel")
-
-    define_option(option_info=proxy_option_info,
-                  option_name="insecure",
-                  help_str="Specify the private key file",
-                  option_type="string",
-                  default="no",
-                  choices=["yes", "no"],
-                  cmd_flags="--insecure")
+                  config_file_flags="advanced:plugins")
 
     define_option(option_info=proxy_option_info,
                   option_name="client_certs",
                   help_str="Specify the location of trusted ca pem file",
-                  option_type="string",
+                  option_type="str",
                   default="",
-                  cmd_flags="--client-certs")
+                  cmd_flags="--client-certs",
+                  config_file_flags="advanced:client.certs")
+
+    define_option(option_info=proxy_option_info,
+                  option_name="insecure",
+                  help_str="This option make the proxy connect to inscure SSL connections.",
+                  option_type="bool",
+                  default=False,
+                  cmd_flags=["--insecure", "-k"])
 
     return proxy_option_info
 
@@ -122,19 +105,21 @@ def create_tui_viewer_options():
     define_option(option_info=tui_viewer_option_info,
                   option_name="viewer_channel",
                   help_str="Specify the viewer channel. ex. tcp://127.0.0.1:5581",
-                  option_type="string",
-                  cmd_flags="--viewer-channel")
+                  option_type="str",
+                  cmd_flags="--viewer-channel",
+                  config_file_flags="channel:viewer")
 
     define_option(option_info=tui_viewer_option_info,
                   option_name="events_channel",
                   help_str="Specify the events channel. ex. tcp://127.0.0.1:5582",
-                  option_type="string",
-                  cmd_flags="--events-channel")
+                  option_type="str",
+                  cmd_flags="--events-channel",
+                  config_file_flags="channel:events")
 
     define_option(option_info=tui_viewer_option_info,
                   option_name="replay_file",
                   help_str="Specify a replay script file",
-                  option_type="string",
+                  option_type="str",
                   default="",
                   cmd_flags="--replay-file")
 
@@ -147,25 +132,28 @@ def create_console_viewer_options():
     define_option(option_info=console_viewer_option_info,
                   option_name="proxy_host",
                   help_str="Specify the proxy host. ex. tcp://127.0.0.1",
-                  option_type="string",
-                  cmd_flags="--proxy-host")
+                  option_type="str",
+                  cmd_flags="--proxy-host",
+                  config_file_flags="channel:host")
 
     define_option(option_info=console_viewer_option_info,
                   option_name="viewer_port",
                   help_str="Specify the viewer channel port. ex. 5581",
                   option_type="int",
-                  cmd_flags="--viewer-channel-port")
+                  cmd_flags="--viewer-channel-port",
+                  config_file_flags="channel:viewer.port")
 
     define_option(option_info=console_viewer_option_info,
                   option_name="events_port",
                   help_str="Specify the events channel port. ex. 5582",
                   option_type="int",
-                  cmd_flags="--events-channel-port")
+                  cmd_flags="--events-channel-port",
+                  config_file_flags="channel:events.port")
 
     define_option(option_info=console_viewer_option_info,
                   option_name="verbose_level",
                   help_str="Specify verbose level. (header, body, all)",
-                  option_type="string",
+                  option_type="str",
                   cmd_flags="--verbose-level",
                   default="status",
                   choices=["status", "header", "body", "all"])
@@ -173,14 +161,14 @@ def create_console_viewer_options():
     define_option(option_info=console_viewer_option_info,
                   option_name="replay_file",
                   help_str="Specify replay file",
-                  option_type="string",
+                  option_type="str",
                   default="",
                   cmd_flags="--replay-file")
 
     define_option(option_info=console_viewer_option_info,
                   option_name="dump_file",
                   help_str="Specify dump file",
-                  option_type="string",
+                  option_type="str",
                   default="",
                   cmd_flags="--dump-file")
 
@@ -216,23 +204,28 @@ def run_proxy_mode(config):
         logger.info("bye")
 
 
-def main():  # pragma: no cover
+def mpserver():  # pragma: no cover
     # NOTE: logger must start before everything else
     from microproxy.utils import init_system_logger
     init_system_logger()
 
-    config_field_info = create_options()
+    config_field_info = create_proxy_options()
     config = parse_config(config_field_info)
 
-    if config["command_type"] == "proxy":
-        run_proxy_mode(config)
-    elif config["command_type"] == "console-viewer":
-        from microproxy.viewer import console as console_viewer
-        console_viewer.start(config)
-    elif config["command_type"] == "tui-viewer":
-        from microproxy.viewer import tui as tui_viewer
-        tui_viewer.start(config)
+    run_proxy_mode(config)
 
 
-if __name__ == '__main__':
-    main()
+def mpdump(): # pragma: no cover
+    from microproxy.viewer import console as console_viewer
+
+    config_field_info = create_console_viewer_options()
+    config = parse_config(config_field_info)
+    console_viewer.start(config)
+
+
+def mptui(): # pragma: no cover
+    from microproxy.viewer import tui as tui_viewer
+
+    config_field_info = create_tui_viewer_options()
+    config = parse_config(config_field_info)
+    tui_viewer.start(config)
