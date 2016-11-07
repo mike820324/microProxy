40a8a74ce152d5222ff37057eebe0277c475a6a9
MicroMike <mike820324@gmail.com>
2016-11-07 10:04:18 +0800

    Add __neq__ to coverage ignore list.

--- microproxy/context/base.py
+++ microproxy/context/base.py
@@ -20,7 +20,7 @@ class Serializable(object):
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
 
-    def __neq__(self, other):  # pragma: no cover
+    def __neq__(self, other):
         return not self.__eq__(other)
 
 
