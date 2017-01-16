7057c51b66c59cbcbbd3cebd10d947cd50685345
MicroMike <mike820324@gmail.com>
2017-01-16 16:14:43 +0800

    Sync the stream id between client and server.
    - Remove some of the code from application/http2.py.

--- microproxy/layer/application/http2.py
+++ microproxy/layer/application/http2.py
@@ -33,8 +33,6 @@ class Http2Layer(ApplicationLayer):
             on_terminate=self.on_dest_terminate,
             on_reset=self.on_dest_reset)
         self.streams = dict()
-        self.src_to_dest_ids = dict([(0, 0)])
-        self.dest_to_src_ids = dict([(0, 0)])
         self._future = concurrent.Future()
 
     @gen.coroutine
@@ -71,25 +69,17 @@ class Http2Layer(ApplicationLayer):
         if self._future.running():
             self._future.set_result(self.context)
 
-    def update_ids(self, src_stream_id, dest_stream_id):
-        self.src_to_dest_ids[src_stream_id] = dest_stream_id
-        self.dest_to_src_ids[dest_stream_id] = src_stream_id
-
     def on_request(self, stream_id, request, priority_updated):
-        dest_stream_id = self.dest_conn.get_next_available_stream_id()
-        self.update_ids(stream_id, dest_stream_id)
-
         if priority_updated:
             priority_weight = priority_updated.weight
             priority_exclusive = priority_updated.exclusive
-            priority_depends_on = self.safe_mapping_id(
-                self.src_to_dest_ids, priority_updated.depends_on)
+            priority_depends_on = priority_updated.depends_on
         else:
             priority_weight = None
             priority_exclusive = None
             priority_depends_on = None
 
-        stream = Stream(self, self.context, stream_id, dest_stream_id)
+        stream = Stream(self, self.context, stream_id)
         stream.on_request(
             request,
             priority_weight=priority_weight,
@@ -98,18 +88,14 @@ class Http2Layer(ApplicationLayer):
         self.streams[stream_id] = stream
 
     def on_push(self, pushed_stream_id, parent_stream_id, request):
-        self.update_ids(pushed_stream_id, pushed_stream_id)
-        target_parent_stream_id = self.dest_to_src_ids[parent_stream_id]
-
-        stream = Stream(self, self.context, pushed_stream_id, pushed_stream_id)
-        stream.on_push(request, target_parent_stream_id)
+        stream = Stream(self, self.context, pushed_stream_id)
+        stream.on_push(request, parent_stream_id)
         self.streams[pushed_stream_id] = stream
 
     def on_response(self, stream_id, response):
-        src_stream_id = self.dest_to_src_ids[stream_id]
-        self.streams[src_stream_id].on_response(response)
+        self.streams[stream_id].on_response(response)
 
-        self.on_finish(src_stream_id)
+        self.on_finish(stream_id)
 
     def on_finish(self, src_stream_id):
         stream = self.streams[src_stream_id]
@@ -136,35 +122,26 @@ class Http2Layer(ApplicationLayer):
         self.src_conn.send_update_settings(new_settings)
 
     def on_src_window_updates(self, stream_id, delta):
-        target_stream_id = self.safe_mapping_id(self.src_to_dest_ids, stream_id)
+        target_stream_id = stream_id
         self.dest_conn.send_window_updates(target_stream_id, delta)
 
     def on_dest_window_updates(self, stream_id, delta):
-        target_stream_id = self.safe_mapping_id(self.dest_to_src_ids, stream_id)
+        target_stream_id = stream_id
         self.src_conn.send_window_updates(target_stream_id, delta)
 
     def on_src_priority_updates(self, stream_id, depends_on,
                                 weight, exclusive):
-        target_stream_id = self.safe_mapping_id(
-            self.src_to_dest_ids, stream_id)
-        target_depends_on = self.safe_mapping_id(
-            self.src_to_dest_ids, depends_on)
+        target_stream_id = stream_id
+        target_depends_on = depends_on
         if target_stream_id:
             self.dest_conn.send_priority_updates(
                 target_stream_id, target_depends_on, weight, exclusive)
 
-    def safe_mapping_id(self, ids, stream_id):
-        if stream_id in ids:
-            return ids[stream_id]
-        return 0
-
     def on_src_reset(self, stream_id, error_code):
-        target_stream_id = self.src_to_dest_ids[stream_id]
-        self.dest_conn.send_reset(target_stream_id, error_code)
+        self.dest_conn.send_reset(stream_id, error_code)
 
     def on_dest_reset(self, stream_id, error_code):
-        target_stream_id = self.dest_to_src_ids[stream_id]
-        self.src_conn.send_reset(target_stream_id, error_code)
+        self.src_conn.send_reset(stream_id, error_code)
 
     def on_src_terminate(self, additional_data, error_code, last_stream_id):
         self.dest_conn.send_terminate(
@@ -180,29 +157,28 @@ class Http2Layer(ApplicationLayer):
 
 
 class Stream(object):
-    def __init__(self, layer, context, src_stream_id, dest_stream_id):
+    def __init__(self, layer, context, stream_id):
         self.layer = layer
         self.context = context
-        self.src_stream_id = src_stream_id
-        self.dest_stream_id = dest_stream_id
+        self.stream_id = stream_id
         self.request = None
         self.response = None
 
     def on_request(self, request, **kwargs):
-        plugin_ressult = self.layer.interceptor.request(
+        plugin_result = self.layer.interceptor.request(
             layer_context=self.context, request=request)
 
-        self.request = plugin_ressult.request if plugin_ressult else request
+        self.request = plugin_result.request if plugin_result else request
         self.layer.dest_conn.send_request(
-            self.dest_stream_id, self.request, **kwargs)
+            self.stream_id, self.request, **kwargs)
 
     def on_push(self, request, parent_stream_id):
-        plugin_ressult = self.layer.interceptor.request(
+        plugin_result = self.layer.interceptor.request(
             layer_context=self.context, request=request)
 
-        self.request = plugin_ressult.request if plugin_ressult else request
+        self.request = plugin_result.request if plugin_result else request
         self.layer.src_conn.send_pushed_stream(
-            parent_stream_id, self.src_stream_id, self.request)
+            parent_stream_id, self.stream_id, self.request)
 
     def on_response(self, response):
         plugin_result = self.layer.interceptor.response(
@@ -212,4 +188,4 @@ class Stream(object):
 
         self.response = plugin_result.response if plugin_result else response
         self.layer.src_conn.send_response(
-            self.src_stream_id, self.response)
+            self.stream_id, self.response)
